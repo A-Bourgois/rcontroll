b28de37d7c8ddc2f8b71ceae6faf8b4dc1a44ebe
A-Bourgois <audrey.bourgois01@gmail.com>
2024-07-05 11:00:52 +0200

    suite Version finale TROLL Audrey

--- tests/testthat/test-cpp.R
+++ tests/testthat/test-cpp.R
@@ -8,12 +8,15 @@ test_that("cpp", {
     name = "test",
     global = generate_parameters(
       rows = 100, cols = 100,
-      iterperyear = 12, nbiter = 4
+      iterperyear = 12, nbiter = 4,
+      Rrecruit = 1,
+      fecundity = 1,
+      distdisperse = 1
     ),
     species = TROLLv3_species,
     climate = TROLLv3_climatedaytime12,
     daily = TROLLv3_daytimevar,
-    forest = get_forest(TROLLv3_output),
+    # forest = get_forest(TROLLv3_output), # pour faire des tests à partir d'une forêt mature, à supprimer si on veut partir de 0
     verbose = TRUE
   )
   expect_s4_class(sim, "trollsim")
